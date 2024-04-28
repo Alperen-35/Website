893f58611eabe5863dfce26a06a72a8d5778ba4b
Alperen <86462498+Alperen-35@users.noreply.github.com>
2024-04-28 23:57:01 +0300

    İlk

---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Alperen | Website">
+    <meta property="og:image"  />
+    <meta name="theme-color" content="#2c94a4">
+    <meta name="keywords" content=", Web Developer, Python, Apps, Developer, Back End Developer">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Alperen | Website</title>
+	<link rel="canonical" href="https://sans.zencigot.com.tr/" /> 
+    <link rel="icon" href="alperen.png">
+    <link rel="stylesheet" href="css/styles.min.css">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118887921-1"></script>
+    <script src="js/scripts.min.js" async></script>
+</head>
+<body>
+    <main>
+        <div class="logo-overlay"></div><img class="tcf-logo" src="img/lol.gif" alt="Logo"><span class="page-state pulse">Loading...</span>
+        <div class="room"><img class="room-gif" src="img/room.gif" alt="Room"><svg class="monitor-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 600 400" style="enable-background:new 0 0 600 400;" xml:space="preserve"><g class="monitor"><rect class="st1" x="79.5" y="157.5" width="81" height="49"></rect></g></svg>
+            <div class="interactive-wrap">
+                <div class="monitor"></div>
+            </div>
+        </div>
+        <div class="screenGrid"></div>
+        <div class="screenGrid"></div>
+        <div class="screenGrid"></div>
+        <div class="screen desktop">
+            <div class="noise"></div>
+            <div class="monitor-edges"></div>
+            <div class="apps">
+                <div class="app-icon work"><i class="fa fa-folder fa-2x"></i><span>PROJELER</span></div>
+                <div class="clearfix"></div>
+                <div class="app-icon about"><i class="fa fa-user-circle fa-2x"></i><span>HAKKIMDA</span></div>
+                <div class="clearfix"></div>
+                <div class="app-icon contact"><a href="mailto: alperenfidan600@gmail.com" target="_blank" rel="noopener"><i class="fa fa-envelope fa-2x"></i><span>İLETİŞİM</span></a></div>
+                <div class="clearfix"></div>
+                <div class="clearfix">
+                <div class="app-icon trash"><i class="fa fa-trash fa-2x"></i><span>ÇÖP</span></div>
+                    </i><span></span></div>
+            </div>
+            <div class="apps social">
+                <div class="app-icon social"><a href="https://github.com/Alperen-35" target="_blank" rel="noopener"><i class="fa fa-github fa-2x"></i><span>Github</span></a></div>
+                <div class="clearfix"></div>
+                <div class="app-icon social"><a href="https://replit.com/@kundakci" target="_blank" rel="noopener"><i class="fa fa-code fa-2x"></i><span>Replit</span></a></div>
+                <div class="clearfix"></div>
+                <div class="app-icon social"><a href="https://open.spotify.com/user/3176kbglozj5e7w5b2tighvlqjrq?si=ddac54385fa249e5" target="_blank" rel="noopener"><i class="fa fa-spotify fa-2x"></i><span>Spotify</span></a></div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="explorer-window trash-explorer" tabindex="0">
+                <div class="window-header">
+                    <p class="window-title">Çöp</p>
+                    <div class="button-wrap close-explorer"><i class="fa fa-times"></i></div>
+                </div>
+                <div class="window-body trash-body">
+                    <div class="clearfix"></div>
+                    <div class="explorer-icon ie"><i class="fa fa-internet-explorer fa-2x"></i></div>
+                </div>
+            </div>
+            <div class="explorer-window about-explorer" tabindex="0">
+                <div class="window-header">
+                    <p class="window-title">Hakkımda</p>
+                    <div class="button-wrap close-explorer"><i class="fa fa-times"></i></div>
+                </div>
+                <div class="window-body about-body">
+                    <div class="clearfix"></div>
+                    <p>Ben <a href="mailto: alperenfidan600@gmail.com" target="_blank" rel="noopener">Alperen</a>👋🏼,</p>
+                    <p>Yazılım ve siber güvenlik alanlarına meraklı biriyim.</p>
+                    <p>Hobilerim arasında kod yazmak, siber güvenlikle uğraşmak yer alıyor. Amacım, bu alanlarda kendimi daha da geliştirerek ileride profesyonel bir yazılımcı olmak.</p>
+                </div>
+            </div>
+            <div class="explorer-window work-explorer" tabindex="0">
+                <div class="window-header">
+                    <p class="window-title">Projeler</p>
+                    <div class="button-wrap close-explorer"><i class="fa fa-times"></i></div>
+                </div>
+                <div class="window-body work-body">
+                    <div class="work-item">
+                        <a class="work-link-1" href="https://github.com/Alperen-35/qrmenu" target="_blank"> <img src="github.png"><span class="name">QR Menü Websitesi</span><span class="language two no-left-margin">Laravel</span><span class="language two">Yazılım</span></a>
+                    </div>
+                    <div class="work-item">
+                        <a class="work-link-2" href="https://replit.com/@kundakci/on-kere-arayan-birlesik-krallik-aramasi?v=1#main.py" target="_blank"> <img src="github.png"><span class="name">Call Bomber (10 Kere)</span><span class="language two no-left-margin">Python</span><span class="language two">Yazılım</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="info">
+                <div>
+                    <p class="time"><span id="current-time"></span></p>
+                    <p class="date"><span id="current-date"></span></p>
+                </div><i class="fa fa-power-off fa-2x shutdown info-icon"></i></div>
+        </div>
+    </main>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1GQE18XFWX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-1GQE18XFWX');
+</script>
+</html>
